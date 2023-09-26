b3980db9e15dca35bcf1775dd455fb684213c7f4
Sakib Islam <bachelorsakib@gmail.com>
2023-09-26 18:41:19 +0600

    donation added

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -23,31 +23,34 @@ const Donation = () => {
 
     return (
         <div>
-            Donation page
+            
             {
                 noFound?(<p className="text-2xl font-semibold flex justify-center items-center mt-20">{noFound}</p>) : (
-                    <div>
+                    <div >
                         {donateItem.length>0 && (
-                            <div className='flex justify-center items-center mt-10'>
+                            <div className='flex gap-5 justify-center items-center mt-10'>
                            {
                             isShow? donateItem.map(item=>(
-                                <ShowDonate key={item.id}></ShowDonate>
+                                <ShowDonate key={item.id} item={item}></ShowDonate>
                             ) 
 
                                 )
                                 
                             
                             : 
-                                donateItem.slice(0,4).map(item=>(<ShowDonate key={item.id}></ShowDonate>))
+                                donateItem.slice(0,2).map(item=>(<ShowDonate item={item} key={item.id}></ShowDonate>))
                            }
-
-{
-                            donateItem.length>4 && <button onClick={()=>setIsShow(!isShow)}
-
+                            <div>
+                            {
+                            donateItem.length>2 && <button onClick={()=>setIsShow(!isShow)}
+                                className="p-2 bg-green-400 rounded text-xl"
                             >  {isShow? "See less" : "See more"}
                             </button>
                         }
+                            </div>
+
                         </div>
+                        
 
                          
                        
--- src/component/ShowDonate/ShowDonate.jsx
+++ src/component/ShowDonate/ShowDonate.jsx
@@ -1,9 +1,24 @@
 
 
-const ShowDonate = () => {
+const ShowDonate = ({item}) => {
+    const { id, title, price, picture } = item;
+
     return (
         <div>
-            <h1>donate items here</h1>
+            {/* <h1>donate items here</h1> */}
+            <div className=' '>
+                <div>
+                <img src={picture} alt="" />
+                <p className='text-2xl font-bold'>{title}</p>
+                
+               
+               <button className="btn bg-green-400 p-4">
+                    Donate ${price}
+                </button>
+               
+                </div>
+            </div>
+
         </div>
     );
 };
